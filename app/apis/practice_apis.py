933ec5bda46c9d0517d6ada97f2c6b5d94330428
fabxoe <fabxoe.kor@gmail.com>
2026-07-14 15:25:55 +0900

    Add practice API scaffold

---
+++ app/apis/practice_apis.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, Path
+
+app = FastAPI()
+
+user_list = [
+	{
+		"id": 1,
+		"name": "홍길동",
+		"age": 24,
+		"email": "gildong24@example.com",
+		"password": "Password1234!!"
+	},
+	{
+		"id": 2,
+		"name": "장문복",
+		"age": 21,
+		"email": "moonluck12@example.com",
+		"password": "Check1321!"
+	},
+	{
+		"id": 3,
+		"name": "임우진",
+		"age": 31,
+		"email": "limousine33@example.com",
+		"password": "lwsPAssword12@"
+	}
+]

a660483ccac04edb044c9e0802c0d047a415842d
ts04042 <ts04042@gamil.com>
2026-07-21 15:16:09 +0900

    feat: 환자 목록 조회  API

--- app/apis/patient_api.py
+++ app/apis/patient_api.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Query, Response, status
+from fastapi import APIRouter, Depends, Query, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db.databases import async_get_db
@@ -7,7 +7,6 @@ from app.models.user import Gender, Role, User
 from app.schemas.patient import (
     PatientCreateRequest,
     PatientResponse,
-    PatientUpdateRequest,
 )
 from app.services.patient_service import PatientService
 
@@ -30,4 +29,26 @@ async def create_patient(
     return await PatientService.create(
         db,
         payload,
+    )
+
+
+@router.get(
+    "",
+    response_model=list[PatientResponse],
+    status_code=status.HTTP_200_OK,
+)
+async def get_patients(
+    name: str | None = Query(default=None),
+    gender: Gender | None = Query(default=None),
+    min_age: int | None = Query(default=None),
+    max_age: int | None = Query(default=None),
+    db: AsyncSession = Depends(async_get_db),
+    current_user: User = Depends(get_current_user),
+):
+    return await PatientService.get_all(
+        db,
+        name=name,
+        gender=gender,
+        min_age=min_age,
+        max_age=max_age,
     )
--- app/services/patient_service.py
+++ app/services/patient_service.py
@@ -1,13 +1,9 @@
-from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models.patient import Patient
-from app.repositories.patient_repository import PatientRepository
-from app.schemas.patient import (
-    PatientCreateRequest,
-    PatientUpdateRequest,
-)
 from app.models.user import Gender
+from app.repositories.patient_repository import PatientRepository
+from app.schemas.patient import PatientCreateRequest
 
 
 class PatientService:
@@ -28,4 +24,21 @@ class PatientService:
         return await PatientRepository.create(
             db,
             patient,
+        )
+
+    @staticmethod
+    async def get_all(
+        db: AsyncSession,
+        name: str | None = None,
+        gender: Gender | None = None,
+        min_age: int | None = None,
+        max_age: int | None = None,
+    ) -> list[Patient]:
+
+        return await PatientRepository.get_all(
+            db,
+            name=name,
+            gender=gender,
+            min_age=min_age,
+            max_age=max_age,
         )
